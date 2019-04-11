2427156aa19713398b2056b55b9892bad1861fa0
Maxime M <maxime.moineau@free.fr>
2019-04-11 22:24:42 +0200

    Add pom, & repo du sujet
    (normalement on peut git pull le repo du sujet)

---
+++ src/main/java/M1_MIAGE/App.java
@@ -0,0 +1,13 @@
+package M1_MIAGE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
