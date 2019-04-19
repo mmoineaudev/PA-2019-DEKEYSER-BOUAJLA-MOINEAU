f416d1ec31249d514213ab4cd3caaace0cec78b7
Maxime <maxime.moineau.dev@gmail.com>
2019-04-19 16:43:42 +0200

    add rotation du vaisseau fonction de la direction

--- src/main/java/m1_miage/abstraction/game_objects/VaisseauSprite.java
+++ src/main/java/m1_miage/abstraction/game_objects/VaisseauSprite.java
@@ -6,6 +6,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+import javafx.scene.transform.Rotate;
 import m1_miage.abstraction.Sprite;
 import m1_miage.abstraction.game_objects.navigation.Direction;
 import m1_miage.presenter.GameBoard;
@@ -28,9 +29,11 @@ public class VaisseauSprite extends IntelligentSprite {
     @Override
     public void update(double time, GameBoard b) {
         speed+=1;
-        this.direction = Direction.values()[(int) (Math.random()*Direction.values().length)];
+        setDirection(Direction.values()[(int) (Math.random()*Direction.values().length)]);
         super.update(time,b);
     }
+
+
 
     @Override
     public Shape getBoundingShape() {
@@ -40,8 +43,13 @@ public class VaisseauSprite extends IntelligentSprite {
     @Override
     public void render(GraphicsContext gc) {
         Paint save = gc.getFill();
-        gc.drawImage(image, x, y);
+        //gc.drawImage(image, x, y);
+        drawRotatedImage(gc, image, getAngle(), x,y);
         gc.setFill(save);
+    }
+    //TODO test
+    private double getAngle() {
+        return direction.getValue()*90;
     }
 
     @Override
@@ -51,4 +59,31 @@ public class VaisseauSprite extends IntelligentSprite {
         if(lifes==0) System.out.println(" Le vaisseau a explosé :'( ");
 
     }
+
+
+    //-- TODO refactor vers gameboard
+    /**
+     * source : https://stackoverflow.com/questions/18260421/how-to-draw-image-rotated-on-javafx-canvas
+     * Draws an image on a graphics context.
+     *
+     * The image is drawn at (tlpx, tlpy) rotated by angle pivoted around the point:
+     *   (tlpx + image.getWidth() / 2, tlpy + image.getHeight() / 2)
+     *
+     * @param gc the graphics context the image is to be drawn on.
+     * @param angle the angle of rotation.
+     * @param tlpx the top left x co-ordinate where the image will be plotted (in canvas co-ordinates).
+     * @param tlpy the top left y co-ordinate where the image will be plotted (in canvas co-ordinates).
+     */
+    private void drawRotatedImage(GraphicsContext gc, Image image, double angle, double tlpx, double tlpy) {
+        gc.save(); // saves the current state on stack, including the current transform
+        rotate(gc, angle, tlpx + image.getWidth() / 2, tlpy + image.getHeight() / 2);
+        gc.drawImage(image, tlpx, tlpy);
+        gc.restore(); // back to original state (before rotation)
+    }
+    private void rotate(GraphicsContext gc, double angle, double px, double py) {
+        Rotate r = new Rotate(angle, px, py);
+        gc.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
+    }
+
+
 }
--- src/main/java/m1_miage/abstraction/game_objects/navigation/Direction.java
+++ src/main/java/m1_miage/abstraction/game_objects/navigation/Direction.java
@@ -4,5 +4,18 @@ package m1_miage.abstraction.game_objects.navigation;
  * On va donner une direction aux vaisseaux pour les orienter
  */
 public enum  Direction {
-    SOUTH, NORTH, EAST, WEST;
+    NORTH(0),
+    EAST(1),
+    SOUTH(2),
+    WEST(3) ;
+
+    private int value;
+
+    Direction(int i) {
+        this.value = i;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
