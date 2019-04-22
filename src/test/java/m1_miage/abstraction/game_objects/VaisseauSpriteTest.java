c44ddffefc146cb1d55898b27e0aeb44ea9f0d49
Maxime <maxime.moineau.dev@gmail.com>
2019-04-22 11:27:06 +0200

    remove makeeverythingpublic
    on va tester normalement déjà, je completerai si j'ai le temps

--- src/test/java/m1_miage/abstraction/game_objects/VaisseauSpriteTest.java
+++ src/test/java/m1_miage/abstraction/game_objects/VaisseauSpriteTest.java
@@ -2,7 +2,6 @@ package m1_miage.abstraction.game_objects;
 
 import org.junit.Before;
 import org.junit.Test;
-import tools.MakeEveryThingPublic;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -15,27 +14,8 @@ public class VaisseauSpriteTest {
     private Object vaisseauSprite;
     @Before
     public void init(){
-        Class c;
-        Constructor[] classConstructor;
-        try {
-            c = MakeEveryThingPublic.getClassWithPublicAccess("m1_miage.abstraction.game_objects.VaisseauSprite");
-            System.out.println("c = " + c);
-            classConstructor = getClass().getDeclaredConstructors();
-            System.out.println("classConstructor = " + classConstructor);
-            vaisseauSprite = classConstructor[0].newInstance();
-            System.out.println("vaisseauSprite = " + vaisseauSprite.getClass().getSimpleName());//we have a problem
-        }catch(Exception e) {
-            System.out.println("EXCEPTION :"+e.getMessage()+"\n"+e.getCause());
 
-            e.printStackTrace();
-        }finally {
-            fail();
-        }
     }
 
-    @Test
-    public void testForMakingEveryThingPublic(){
-        System.out.println("vaisseauSprite = " + vaisseauSprite.getClass().getSimpleName());
-        assertTrue(vaisseauSprite instanceof VaisseauSprite);
-    }
+
 }
